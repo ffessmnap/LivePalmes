b6a7850224e18d811608ce567b96a4b205575dfc
ffessmnap <antoine.fauveau@nap-ffessm.fr>
2026-09-04 23:16:42 +0200

    Run TEST bootstrap test in verification suite

--- tools/verify-livepalmes.js
+++ tools/verify-livepalmes.js
@@ -53,6 +53,7 @@ function runUnitTests() {
     "livepalmes-environment-tests.js",
     "livepalmes-result-regression-tests.js",
     "livepalmes-test-backend-workflow-tests.js",
+    "firebase-test-access-bootstrap-tests.js",
     "livepalmes-public-results-index-tests.js",
     "livepalmes-publication-tests.js",
     "livepalmes-public-pdf-storage-tests.js",
